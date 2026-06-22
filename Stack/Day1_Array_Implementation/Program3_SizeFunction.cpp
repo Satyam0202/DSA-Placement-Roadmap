7464bc93647c8ec1e17204983b56d31c76860691
Satyam0202 <m4740600@gmail.com>
2026-06-22 18:05:40 +0530

    Day 1 complet with basic Stack Operation with Array !

---
+++ Stack/Day1_Array_Implementation/Program3_SizeFunction.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+#define size 5
+int stack[size];
+int top=-1;
+
+bool isEmpty()
+{
+    if(top==-1)
+        return true;
+    else
+        return false;
+}
+bool isFull()
+{
+    if(top==size-1)
+        return true;
+    else
+        return false;
+}
+void push(int element)
+{
+    if(!isFull())
+    {
+        stack[++top]=element;
+        cout<<"Element  Pushhed...\n";
+    }
+    else
+        cout<<"Sorry Stack Is Already Full...\n";
+}
+void pop()
+{
+    if(!isEmpty())
+    {
+        cout<<"Element Poped... "<<stack[top--]<<endl;
+    }
+    else
+        cout<<"Sorry Stack  Is Already Empty....\n";
+}
+void peek()
+{
+    if(!isEmpty())
+        cout<<"Element In Top Is.. \n"<<stack[top]<<endl;
+    else
+        cout<<"Sorry Stack is Already Empty!..\n";
+}
+void show()
+{
+    if(!isEmpty())
+    {
+        for(int i=top;i>=0;i--)
+            cout<<"\n| "<<stack[i]<<" |";
+    }
+    else
+        cout<<"Sorry Stack Is Already Empty!...\n";
+}
+int stack_size()
+{
+    return top+1;
+}
+int main()
+{
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    cout<<"This is Stack ..\n";
+    show();
+    cout<<endl;
+
+    cout<<"\nSize is"<<stack_size();  
+    return 0;
+}
